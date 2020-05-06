a94f8f24a69fca08fe2aaf9ba3448abb618eb78b
S0m3randomguy <63020240+S0m3randomguy@users.noreply.github.com>
2020-05-06 18:32:58 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,10 +3,73 @@ enum JacDacMessage {
 }
 namespace SpriteKind {
     export const Screen = SpriteKind.create()
+    export const Logo = SpriteKind.create()
 }
-controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-	
-})
+function serviceManager () {
+    if (powerState == 0) {
+        jacdac.messageBusService.start()
+    } else if (powerState == 1) {
+        jacdac.messageBusService.stop()
+    }
+}
+function startupScreen () {
+    scene.setBackgroundColor(7)
+    mySprite = sprites.create(img`
+. . . . . . . . . . . . . . . f f f f f f f f f f f f f f f f f f f f f . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . f f f f f f f f f f f f f f f f f f f f f . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . f f f f f f f f f f f f f f f f f f f f f . . . . . . . . . . . . 
+. . . . . . . . . f f f f f f 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 f f f f f f . . . . . . 
+. . . . . . . . . f f f f f f 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 f f f f f f . . . . . . 
+. . . . . . . . . f f f f f f 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 f f f f f f . . . . . . 
+. . . . . . f f f 1 1 1 1 1 1 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 1 1 1 1 1 1 f f f . . . 
+. . . . . . f f f 1 1 1 1 1 1 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 1 1 1 1 1 1 f f f . . . 
+. . . . . . f f f 1 1 1 1 1 1 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 1 1 1 1 1 1 f f f . . . 
+. . . . . . f f f 1 1 1 1 1 1 1 1 1 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 1 1 1 1 1 1 1 1 1 f f f . . . 
+. . . . . . f f f 1 1 1 1 1 1 1 1 1 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 1 1 1 1 1 1 1 1 1 f f f . . . 
+. . . . . . f f f 1 1 1 1 1 1 1 1 1 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 1 1 1 1 1 1 1 1 1 f f f . . . 
+. . . f f f 9 9 9 7 7 7 7 7 7 1 1 1 1 1 1 7 7 7 7 7 7 7 7 7 1 1 1 1 1 1 7 7 7 7 7 7 9 9 9 f f f 
+. . . f f f 9 9 9 7 7 7 7 7 7 1 1 1 1 1 1 7 7 7 7 7 7 7 7 7 1 1 1 1 1 1 7 7 7 7 7 7 9 9 9 f f f 
+. . . f f f 9 9 9 7 7 7 7 7 7 1 1 1 1 1 1 7 7 7 7 7 7 7 7 7 1 1 1 1 1 1 7 7 7 7 7 7 9 9 9 f f f 
+. . . f f f 9 9 9 7 7 7 7 7 7 7 7 7 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 7 7 7 7 7 7 7 7 7 9 9 9 f f f 
+. . . f f f 9 9 9 7 7 7 7 7 7 7 7 7 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 7 7 7 7 7 7 7 7 7 9 9 9 f f f 
+. . . f f f 9 9 9 7 7 7 7 7 7 7 7 7 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 7 7 7 7 7 7 7 7 7 9 9 9 f f f 
+. . . f f f 9 9 9 7 7 7 7 7 7 7 7 7 7 7 7 1 1 1 1 1 1 1 1 1 7 7 7 7 7 7 7 7 7 7 7 7 9 9 9 f f f 
+. . . f f f 9 9 9 7 7 7 7 7 7 7 7 7 7 7 7 1 1 1 1 1 1 1 1 1 7 7 7 7 7 7 7 7 7 7 7 7 9 9 9 f f f 
+. . . f f f 9 9 9 7 7 7 7 7 7 7 7 7 7 7 7 1 1 1 1 1 1 1 1 1 7 7 7 7 7 7 7 7 7 7 7 7 9 9 9 f f f 
+. . . f f f 9 9 9 7 7 7 7 7 7 7 7 7 7 7 7 1 1 1 1 1 1 1 1 1 7 7 7 7 7 7 7 7 7 7 7 7 9 9 9 f f f 
+. . . f f f 9 9 9 7 7 7 7 7 7 7 7 7 7 7 7 1 1 1 1 1 1 1 1 1 7 7 7 7 7 7 7 7 7 7 7 7 9 9 9 f f f 
+. . . f f f 9 9 9 7 7 7 7 7 7 7 7 7 7 7 7 1 1 1 1 1 1 1 1 1 7 7 7 7 7 7 7 7 7 7 7 7 9 9 9 f f f 
+. . . f f f 6 6 6 7 7 7 7 7 7 7 7 7 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 7 7 7 7 7 7 7 7 7 6 6 6 f f f 
+. . . f f f 6 6 6 7 7 7 7 7 7 7 7 7 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 7 7 7 7 7 7 7 7 7 6 6 6 f f f 
+. . . f f f 6 6 6 7 7 7 7 7 7 7 7 7 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 7 7 7 7 7 7 7 7 7 6 6 6 f f f 
+. . . f f f 6 6 6 6 6 6 7 7 7 1 1 1 1 1 1 7 7 7 7 7 7 7 7 7 1 1 1 1 1 1 7 7 7 6 6 6 6 6 6 f f f 
+. . . f f f 6 6 6 6 6 6 7 7 7 1 1 1 1 1 1 7 7 7 7 7 7 7 7 7 1 1 1 1 1 1 7 7 7 6 6 6 6 6 6 f f f 
+. . . f f f 6 6 6 6 6 6 7 7 7 1 1 1 1 1 1 7 7 7 7 7 7 7 7 7 1 1 1 1 1 1 7 7 7 6 6 6 6 6 6 f f f 
+. . . f f f 6 6 6 6 6 6 d d d 1 1 1 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 1 1 1 d d d 6 6 6 6 6 6 f f f 
+. . . f f f 6 6 6 6 6 6 d d d 1 1 1 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 1 1 1 d d d 6 6 6 6 6 6 f f f 
+. . . f f f 6 6 6 6 6 6 d d d 1 1 1 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 1 1 1 d d d 6 6 6 6 6 6 f f f 
+. . . . . . f f f d d d d d d 6 6 6 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6 6 6 d d d d d d f f f . . . 
+. . . . . . f f f d d d d d d 6 6 6 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6 6 6 d d d d d d f f f . . . 
+. . . . . . f f f d d d d d d 6 6 6 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6 6 6 d d d d d d f f f . . . 
+. . . . . . f f f d d d 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 d d d f f f . . . 
+. . . . . . f f f d d d 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 d d d f f f . . . 
+. . . . . . f f f d d d 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 d d d f f f . . . 
+. . . . . . . . . f f f f f f 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 f f f f f f . . . . . . 
+. . . . . . . . . f f f f f f 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 f f f f f f . . . . . . 
+. . . . . . . . . f f f f f f 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 f f f f f f . . . . . . 
+. . . . . . . . . . . . . . . f f f f f f f f f f f f f f f f f f f f f . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . f f f f f f f f f f f f f f f f f f f f f . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . f f f f f f f f f f f f f f f f f f f f f . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+`, SpriteKind.Logo)
+}
+function bootLoader () {
+    powerState = 0
+    pause(500)
+    startupScreen()
+}
 function bootScreenConsole () {
     game.setDialogFrame(img`
 f f f f f f f f f f f f f f f 
@@ -45,8 +108,10 @@ f f f f f f f f f f f f f f f
 `)
     game.setDialogTextColor(1)
     game.showLongText("Xbox 86_32   Build 0.0.1 Press A button      to continue", DialogLayout.Top)
-    screenState = 1
+    bootLoader()
 }
+let mySprite: Sprite = null
+let powerState = 0
 let screenState = 0
 game.setDialogFrame(img`
 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
@@ -67,4 +132,5 @@ game.setDialogFrame(img`
 `)
 game.setDialogTextColor(1)
 game.showLongText("HDMI 2", DialogLayout.Top)
+pause(500)
 bootScreenConsole()
