8b5a2c4a058642cf8ae1d16296aedde8f0ef8fea
S0m3randomguy <63020240+S0m3randomguy@users.noreply.github.com>
2020-05-06 20:17:48 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,16 @@
+enum ActionKind {
+    Walking,
+    Idle,
+    Jumping
+}
 enum JacDacMessage {
     message1 = 49434
 }
 namespace SpriteKind {
     export const Screen = SpriteKind.create()
     export const Logo = SpriteKind.create()
+    export const Text = SpriteKind.create()
+    export const Tile = SpriteKind.create()
 }
 function serviceManager () {
     if (powerState == 0) {
@@ -12,9 +19,66 @@ function serviceManager () {
         jacdac.messageBusService.stop()
     }
 }
+function bootLoader () {
+    powerState = 0
+    pause(500)
+    startupScreen()
+}
+function startScreen () {
+    xboxLogo.destroy()
+    xboxText.destroy()
+    scene.setBackgroundColor(7)
+    color.FadeToWhite.startScreenEffect()
+    setupTile = sprites.create(img`
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+6 6 6 7 7 7 7 7 7 7 7 f f f 7 7 7 f f f 7 7 7 f f f 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6 6 6 
+6 6 6 7 7 7 7 7 7 7 7 f f f 7 7 7 f f f 7 7 7 f f f 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6 6 6 
+6 6 6 7 7 7 7 7 7 7 7 f f f 7 7 7 f f f 7 7 7 f f f 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6 6 6 
+6 6 6 7 7 7 7 7 7 7 7 f f f 7 7 7 f f f 7 7 7 f f f 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6 6 6 
+6 6 6 7 7 7 7 7 7 7 7 f f f 7 7 7 f f f 7 7 7 f f f 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6 6 6 
+6 6 6 7 7 7 7 7 7 7 7 f f f 7 7 7 f f f 7 7 7 f f f 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6 6 6 
+6 6 6 7 7 7 7 7 7 7 7 f f f 7 7 7 f f f 7 7 7 f f f 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6 6 6 
+6 6 6 7 7 7 7 7 7 7 7 f f f 7 7 7 f f f 7 7 7 f f f 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6 6 6 
+6 6 6 7 7 7 7 7 7 7 7 f f f f f f f f f 7 7 7 f f f 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6 6 6 
+6 6 6 7 7 7 7 7 7 7 7 f f f f f f f f f 7 7 7 f f f 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6 6 6 
+6 6 6 7 7 7 7 7 7 7 7 f f f f f f f f f 7 7 7 f f f 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6 6 6 
+6 6 6 7 7 7 7 7 7 7 7 f f f 7 7 7 f f f 7 7 7 f f f 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6 6 6 
+6 6 6 7 7 7 7 7 7 7 7 f f f 7 7 7 f f f 7 7 7 f f f 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6 6 6 
+6 6 6 7 7 7 7 7 7 7 7 f f f 7 7 7 f f f 7 7 7 f f f 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6 6 6 
+6 6 6 7 7 7 7 7 7 7 7 f f f 7 7 7 f f f 7 7 7 f f f 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6 6 6 
+6 6 6 7 7 7 7 7 7 7 7 f f f 7 7 7 f f f 7 7 7 f f f 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6 6 6 
+6 6 6 7 7 7 7 7 7 7 7 f f f 7 7 7 f f f 7 7 7 f f f 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6 6 6 
+6 6 6 7 7 7 7 7 7 7 7 f f f 7 7 7 f f f 7 7 7 f f f 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 6 6 6 
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 6 
+`, SpriteKind.Tile)
+}
 function startupScreen () {
     scene.setBackgroundColor(7)
-    mySprite = sprites.create(img`
+    xboxText = sprites.create(img`
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . 1 . . . 1 . 1 1 1 1 . . . 1 1 1 . . 1 . . . 1 . . . . 1 1 1 . . 1 . . . 1 . 1 1 1 1 1 . . . . . . . . . . . . . 
+. . . . . . . . . 1 . 1 . . 1 . . . 1 . 1 . . . 1 . . 1 . 1 . . . . 1 . . . 1 . 1 1 . . 1 . 1 . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . 1 . . . 1 1 1 1 . . 1 . . . 1 . . . 1 . . . . . 1 . . . 1 . 1 . 1 . 1 . 1 1 1 . . . . . . . . . . . . . . . 
+. . . . . . . . . 1 . 1 . . 1 . . . 1 . 1 . . . 1 . . 1 . 1 . . . . 1 . . . 1 . 1 . . 1 1 . 1 . . . . . . . . . . . . . . . . . 
+. . . . . . . . 1 . . . 1 . 1 . . . 1 . 1 . . . 1 . 1 . . . 1 . . . 1 . . . 1 . 1 . . . 1 . 1 . . . . . . . . . . . . . . . . . 
+. . . . . . . . 1 . . . 1 . 1 . . . 1 . 1 . . . 1 . 1 . . . 1 . . . 1 . . . 1 . 1 . . . 1 . 1 . . . . . . . . . . . . . . . . . 
+. . . . . . . . 1 . . . 1 . 1 1 1 1 . . . 1 1 1 . . 1 . . . 1 . . . . 1 1 1 . . 1 . . . 1 . 1 1 1 1 1 . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
+`, SpriteKind.Text)
+    xboxText.setPosition(85, 91)
+    pause(1250)
+    xboxLogo = sprites.create(img`
 . . . . . . . . . . . . . . . f f f f f f f f f f f f f f f f f f f f f . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . f f f f f f f f f f f f f f f f f f f f f . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . f f f f f f f f f f f f f f f f f f f f f . . . . . . . . . . . . 
@@ -64,11 +128,22 @@ function startupScreen () {
 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
 `, SpriteKind.Logo)
-}
-function bootLoader () {
-    powerState = 0
-    pause(500)
-    startupScreen()
+    xboxLogo.setPosition(10, 120)
+    animation.runMovementAnimation(
+    xboxLogo,
+    animation.animationPresets(animation.flyToCenter),
+    250,
+    false
+    )
+    pause(3000)
+    animation.runMovementAnimation(
+    xboxLogo,
+    animation.animationPresets(animation.easeDown),
+    1100,
+    false
+    )
+    pause(1600)
+    startScreen()
 }
 function bootScreenConsole () {
     game.setDialogFrame(img`
@@ -110,7 +185,9 @@ f f f f f f f f f f f f f f f
     game.showLongText("Xbox 86_32   Build 0.0.1 Press A button      to continue", DialogLayout.Top)
     bootLoader()
 }
-let mySprite: Sprite = null
+let setupTile: Sprite = null
+let xboxText: Sprite = null
+let xboxLogo: Sprite = null
 let powerState = 0
 let screenState = 0
 game.setDialogFrame(img`
@@ -132,5 +209,6 @@ game.setDialogFrame(img`
 `)
 game.setDialogTextColor(1)
 game.showLongText("HDMI 2", DialogLayout.Top)
+serviceManager()
 pause(500)
 bootScreenConsole()
